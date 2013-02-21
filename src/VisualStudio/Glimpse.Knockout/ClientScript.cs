e926b5a9f4570f59aaff9215495f47f10030e25e
Aaron Powell <me@aaron-powell.com>
2013-02-21 13:12:40 -0800

    Setting up the NuGet package to load better into glimpse

---
+++ src/VisualStudio/Glimpse.Knockout/ClientScript.cs
@@ -0,0 +1,13 @@
+﻿using Glimpse.Core.Extensibility;
+
+namespace Glimpse.Knockout
+{
+    public sealed class ClientScript : IDynamicClientScript
+    {
+        public ScriptOrder Order { get { return ScriptOrder.IncludeAfterClientInterfaceScript; } }
+        public string GetResourceName()
+        {
+            return "Glimpse.Knockout.Scripts.glimpse-knockout.js";
+        }
+    }
+}
